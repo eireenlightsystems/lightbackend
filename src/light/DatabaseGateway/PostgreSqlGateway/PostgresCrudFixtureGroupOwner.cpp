a792a704130073522a8480e8cecbbdf2f8d3435c
Sergey <rensfo@gmail.com>
2019-03-26 12:16:45 +0300

    clean

--- src/light/DatabaseGateway/PostgreSqlGateway/PostgresCrudFixtureGroupOwner.cpp
+++ src/light/DatabaseGateway/PostgreSqlGateway/PostgresCrudFixtureGroupOwner.cpp
@@ -19,7 +19,7 @@ PostgresCrud<FixtureGroupOwner>::PostgresCrud() {
 Reader<FixtureGroupOwner>::Shared PostgresCrud<FixtureGroupOwner>::parse(const QSqlRecord& record) const {
   PostgresCrud<Contragent> contragentCrud;
   auto contragent = contragentCrud.parse(record);
-  FixtureGroupOwnerShared fixtureGroupOwner = FixtureGroupOwnerShared::create(*contragent);
+  auto fixtureGroupOwner = FixtureGroupOwnerShared::create(*contragent);
   return fixtureGroupOwner;
 }
 
